fa4476d185b2a780fd8ce604cca98f3117e51e63
constantm <constantmeiring@gmail.com>
2017-07-31 17:37:46 +0200

    Set root to data point collections index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "data_points#index"
+  root to: "data_point_collections#index"
   resources :data_points
   resources :data_point_collections
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
