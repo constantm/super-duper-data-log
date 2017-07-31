64813261386d5399622fcebd231c28c207d8f2dc
constantm <constantmeiring@gmail.com>
2017-07-31 18:02:43 +0200

    add some validations

--- app/models/data_point.rb
+++ app/models/data_point.rb
@@ -1,3 +1,4 @@
 class DataPoint < ApplicationRecord
   belongs_to :data_point_collection
+  validates :value, presence: true
 end
--- app/models/data_point_collection.rb
+++ app/models/data_point_collection.rb
@@ -1,5 +1,6 @@
 class DataPointCollection < ApplicationRecord
   has_many :data_points
+  validates :title, presence: true
 
   def latest_value
     data_points.try(:last).try(:value) || 'N/A'
